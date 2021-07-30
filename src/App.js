653d78399b8db787ea2bca2ca4d48317e2dd3ab9
girishapple <girishtamreddy026@gmail.com>
2021-07-30 13:26:21 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CountryGuides from './Components/CountryGuides';
+import Home from './Components/Home';
+import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import CityGuides from './Components/CityGuides';
+import Features from './Components/Features';
+import Flight from './Components/Flight';
+import Accomodation from './Components/Accomodation';
+import Food from './Components/Food';
+
+
+
+
+
+const App = () => {
+    return (
+        <Fragment>  
+            
+          <Router> 
+           <ToastContainer/>
+           <Switch> 
+               <Route path="/" exact>
+               <Home/>
+               </Route>
+               <Route path="/CountryGuides" exact>
+               <CountryGuides/>
+               </Route>
+               <Route path="/CityGuides" exact>
+               <CityGuides/>
+               </Route>
+               <Route path="/features" exact>
+               <Features/>
+               </Route>
+               <Route path="/flights" exact>
+               <Flight/>
+               </Route>
+               <Route path="/accomodation" exact>
+               <Accomodation/>
+               </Route> 
+           </Switch>
+           </Router>
+        </Fragment>
+    )
 }
 
-export default App;
+export default App
